ac6284add1f521ac3738598839a2976091e0af97
Jure Mav <jure.mav@gmail.com>
2013-05-11 16:17:23 +0200

    Added automatic level detection to connect-logger, depends on http status response. Update of connect logger example code, compatible with express 3.x

--- examples/example-connect-logger.js
+++ examples/example-connect-logger.js
@@ -1,14 +1,46 @@
-var log4js = require('./lib/log4js');
-log4js.addAppender(log4js.fileAppender('cheese.log'), 'cheese');
+//The connect/express logger was added to log4js by danbell. This allows connect/express servers to log using log4js.
+//https://github.com/nomiddlename/log4js-node/wiki/Connect-Logger
 
-var logger = log4js.getLogger('cheese');
-logger.setLevel('INFO');
+// load modules
+var log4js = require('log4js');
+var express = require("express");
+var app = express();
 
-var app = require('express').createServer();
+//config
+log4js.configure({
+	appenders: [
+		{ type: 'console' },
+		{ type: 'file', filename: 'logs/log4jsconnect.log', category: 'log4jslog' }
+	]
+});
+
+//define logger
+var logger = log4js.getLogger('log4jslog');
+
+// set at which time msg is logged print like: only on error & above
+// logger.setLevel('ERROR');
+
+//express app
 app.configure(function() {
-    app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO }));
+	app.use(express.favicon(''));
+	// app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO }));
+	// app.use(log4js.connectLogger(logger, { level: 'auto', format: ':method :url :status' }));
+
+	//### AUTO LEVEL DETECTION
+	//http responses 3xx, level = WARN
+	//http responses 4xx & 5xx, level = ERROR
+	//else.level = INFO
+	app.use(log4js.connectLogger(logger, { level: 'auto' }));
 });
-app.get('*', function(req,res) {
-    res.send('hello world\n <a href="/cheese">cheese</a>\n');
+
+//route
+app.get('/', function(req,res) {
+	res.send('hello world');
 });
+
+//start app
 app.listen(5000);
+
+console.log('server runing at localhost:5000');
+console.log('Simulation of normal response: goto localhost:5000');
+console.log('Simulation of error response: goto localhost:5000/xxx');
