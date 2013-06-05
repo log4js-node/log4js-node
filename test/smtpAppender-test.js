11fe5bde5f0dfc0c009c11ece0f21ecbd870553c
Gareth Jones <gareth.jones@sensis.com.au>
2013-06-05 18:30:11 +1000

    increased test coverage for smtp appender

--- test/smtpAppender-test.js
+++ test/smtpAppender-test.js
@@ -21,10 +21,31 @@ function setupLogging(category, options) {
 		}
   };
 
+  var fakeLayouts = {
+    layout: function(type, config) {
+      this.type = type;
+      this.config = config;
+      return log4js.layouts.messagePassThroughLayout;
+    },
+    basicLayout: log4js.layouts.basicLayout,
+    messagePassThroughLayout: log4js.layouts.messagePassThroughLayout
+  };
+
+  var fakeConsole = {
+    errors: [],
+    error: function(msg, value) {
+      this.errors.push({ msg: msg, value: value });
+    }
+  };
+
   var smtpModule = sandbox.require('../lib/appenders/smtp', {
 		requires: {
-      'nodemailer': fakeMailer
-		}
+      'nodemailer': fakeMailer,
+      '../layouts': fakeLayouts
+		},
+    globals: {
+      console: fakeConsole
+    }
   });
 
   log4js.addAppender(smtpModule.configure(options), category);
@@ -32,6 +53,8 @@ function setupLogging(category, options) {
   return {
 		logger: log4js.getLogger(category),
 		mailer: fakeMailer,
+    layouts: fakeLayouts,
+    console: fakeConsole,
 		results: msgs
   };
 }
@@ -91,6 +114,19 @@ vows.describe('log4js smtpAppender').addBatch({
     },
     'message should contain proper data': function (result) {
       checkMessages(result, 'sender@domain.com', 'This is subject');
+    }
+  },
+  'config with layout': {
+    topic: function() {
+      var setup = setupLogging('config with layout', {
+        layout: {
+          type: "tester"
+        }
+      });
+      return setup;
+    },
+    'should configure layout': function(result) {
+      assert.equal(result.layouts.type, 'tester');
     }
   },
   'separate email for each event': {
@@ -164,6 +200,34 @@ vows.describe('log4js smtpAppender').addBatch({
       assert.equal(result.results[1].subject, 'Log event #3');
       assert.ok(new RegExp('.+Log event #3\n$').test(result.results[1].text));
     }
+  },
+  'error when sending email': {
+    topic: function() {
+      var setup = setupLogging('error when sending email', {
+        recipients: 'recipient@domain.com',
+        sendInterval: 0,
+        transport: 'SMTP',
+        SMTP: { port: 25, auth: { user: 'user@domain.com' } }
+      });
+
+      setup.mailer.createTransport = function() {
+        return {
+          sendMail: function(msg, cb) {
+            cb({ message: "oh noes" });
+          },
+          close: function() { }
+        };
+      };
+      
+      setup.logger.info("This will break");
+      return setup.console;
+    },
+    'should be logged to console': function(cons) {
+      assert.equal(cons.errors.length, 1);
+      assert.equal(cons.errors[0].msg, "log4js.smtpAppender - Error happened");
+      assert.equal(cons.errors[0].value.message, 'oh noes');
+    }
   }
+
 }).export(module);
 
