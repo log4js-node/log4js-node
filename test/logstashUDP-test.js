84611d99633238c4a082c8c1970158e613f4f3bc
Sergey Maskalik <mercury2269@gmail.com>
2015-10-12 14:00:21 -0700

    Add test for category

--- test/logstashUDP-test.js
+++ test/logstashUDP-test.js
@@ -42,7 +42,7 @@ function setupLogging(category, options) {
 vows.describe('logstashUDP appender').addBatch({
   'when logging with logstash via UDP': {
     topic: function() {
-      var setup = setupLogging('logstashUDP', {
+      var setup = setupLogging('myCategory', {
         "host": "127.0.0.1",
         "port": 10001,
         "type": "logstashUDP",
@@ -73,6 +73,7 @@ vows.describe('logstashUDP appender').addBatch({
       };
       assert.equal(JSON.stringify(json.fields), JSON.stringify(fields));
       assert.equal(json.message, 'Log event #1');
+      assert.equal(json.category, 'myCategory');
       // Assert timestamp, up to hours resolution.
       var date = new Date(json['@timestamp']);
       assert.equal(
