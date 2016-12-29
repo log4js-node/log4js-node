c196c5bce133053e1e839f1f57c441565c91cc66
Gareth Jones <gareth.jones@shinetech.com>
2016-12-29 18:16:42 +1100

    fix(coverage): exclude node_modules from being covered

--- test/sandbox-coverage.js
+++ test/sandbox-coverage.js
@@ -5,6 +5,9 @@ const sandbox = require('sandboxed-module');
 sandbox.configure({
   sourceTransformers: {
     nyc: function (source) {
+      if (this.filename.indexOf('node_modules') > -1) {
+        return source;
+      }
       const nyc = new (require('nyc'))();
       return nyc.instrumenter().instrumentSync(source, this.filename);
     }
