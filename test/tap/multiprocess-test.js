514fb9c7716b7ea3f43d985ad4d68c6d193469ed
Gareth Jones <gareth.jones@shinetech.com>
2017-02-27 08:25:11 +1100

    fix(test): got multiprocess tests working

--- test/tap/multiprocess-test.js
+++ test/tap/multiprocess-test.js
@@ -2,16 +2,13 @@
 
 const test = require('tap').test;
 const sandbox = require('sandboxed-module');
+const recording = require('../../lib/appenders/recording');
 
 function makeFakeNet() {
   return {
-    logEvents: [],
     data: [],
     cbs: {},
     createConnectionCalled: 0,
-    fakeAppender: function (logEvent) {
-      this.logEvents.push(logEvent);
-    },
     createConnection: function (port, host) {
       const fakeNet = this;
       this.port = port;
@@ -54,27 +51,36 @@ function makeFakeNet() {
 }
 
 test('Multiprocess Appender', (batch) => {
+  batch.beforeEach((done) => {
+    recording.erase();
+    done();
+  });
+
   batch.test('worker', (t) => {
     const fakeNet = makeFakeNet();
 
-    const appender = sandbox.require(
-      '../../lib/appenders/multiprocess',
+    const log4js = sandbox.require(
+      '../../lib/log4js',
       {
         requires: {
           net: fakeNet
         }
       }
-    ).appender({ mode: 'worker', loggerPort: 1234, loggerHost: 'pants' });
+    );
+    log4js.configure({
+      appenders: { worker: { type: 'multiprocess', mode: 'worker', loggerPort: 1234, loggerHost: 'pants' } },
+      categories: { default: { appenders: ['worker'], level: 'trace' } }
+    });
 
-    // don't need a proper log event for the worker tests
-    appender('before connect');
+    const logger = log4js.getLogger();
+    logger.info('before connect');
     fakeNet.cbs.connect();
-    appender('after connect');
+    logger.info('after connect');
     fakeNet.cbs.close(true);
-    appender('after error, before connect');
+    logger.info('after error, before connect');
     fakeNet.cbs.connect();
-    appender('after error, after connect');
-    appender(new Error('Error test'));
+    logger.info('after error, after connect');
+    logger.error(new Error('Error test'));
 
     const net = fakeNet;
     t.test('should open a socket to the loggerPort and loggerHost', (assert) => {
@@ -84,23 +90,23 @@ test('Multiprocess Appender', (batch) => {
     });
 
     t.test('should buffer messages written before socket is connected', (assert) => {
-      assert.equal(net.data[0], JSON.stringify('before connect'));
+      assert.include(net.data[0], JSON.stringify('before connect'));
       assert.end();
     });
 
     t.test('should write log messages to socket as json strings with a terminator string', (assert) => {
-      assert.equal(net.data[0], JSON.stringify('before connect'));
+      assert.include(net.data[0], JSON.stringify('before connect'));
       assert.equal(net.data[1], '__LOG4JS__');
-      assert.equal(net.data[2], JSON.stringify('after connect'));
+      assert.include(net.data[2], JSON.stringify('after connect'));
       assert.equal(net.data[3], '__LOG4JS__');
       assert.equal(net.encoding, 'utf8');
       assert.end();
     });
 
     t.test('should attempt to re-open the socket on error', (assert) => {
-      assert.equal(net.data[4], JSON.stringify('after error, before connect'));
+      assert.include(net.data[4], JSON.stringify('after error, before connect'));
       assert.equal(net.data[5], '__LOG4JS__');
-      assert.equal(net.data[6], JSON.stringify('after error, after connect'));
+      assert.include(net.data[6], JSON.stringify('after error, after connect'));
       assert.equal(net.data[7], '__LOG4JS__');
       assert.equal(net.createConnectionCalled, 2);
       assert.end();
@@ -108,48 +114,53 @@ test('Multiprocess Appender', (batch) => {
 
     t.test('should serialize an Error correctly', (assert) => {
       assert.ok(
-        JSON.parse(net.data[8]).stack,
-        `Expected:\n\n${net.data[8]}\n\n to have a 'stack' property`
+        JSON.parse(net.data[8]).data[0].stack,
+        `Expected:\n\n${net.data[8]}\n\n to have a 'data[0].stack' property`
       );
-      const actual = JSON.parse(net.data[8]).stack;
+      const actual = JSON.parse(net.data[8]).data[0].stack;
       assert.match(actual, /^Error: Error test/);
       assert.end();
     });
+
     t.end();
   });
 
   batch.test('worker with timeout', (t) => {
     const fakeNet = makeFakeNet();
 
-    const appender = sandbox.require(
-      '../../lib/appenders/multiprocess',
+    const log4js = sandbox.require(
+      '../../lib/log4js',
       {
         requires: {
           net: fakeNet
         }
       }
-    ).appender({ mode: 'worker' });
+    );
+    log4js.configure({
+      appenders: { worker: { type: 'multiprocess', mode: 'worker' } },
+      categories: { default: { appenders: ['worker'], level: 'trace' } }
+    });
 
-    // don't need a proper log event for the worker tests
-    appender('before connect');
+    const logger = log4js.getLogger();
+    logger.info('before connect');
     fakeNet.cbs.connect();
-    appender('after connect');
+    logger.info('after connect');
     fakeNet.cbs.timeout();
-    appender('after timeout, before close');
+    logger.info('after timeout, before close');
     fakeNet.cbs.close();
-    appender('after close, before connect');
+    logger.info('after close, before connect');
     fakeNet.cbs.connect();
-    appender('after close, after connect');
+    logger.info('after close, after connect');
 
     const net = fakeNet;
 
     t.test('should attempt to re-open the socket', (assert) => {
       // skipping the __LOG4JS__ separators
-      assert.equal(net.data[0], JSON.stringify('before connect'));
-      assert.equal(net.data[2], JSON.stringify('after connect'));
-      assert.equal(net.data[4], JSON.stringify('after timeout, before close'));
-      assert.equal(net.data[6], JSON.stringify('after close, before connect'));
-      assert.equal(net.data[8], JSON.stringify('after close, after connect'));
+      assert.include(net.data[0], JSON.stringify('before connect'));
+      assert.include(net.data[2], JSON.stringify('after connect'));
+      assert.include(net.data[4], JSON.stringify('after timeout, before close'));
+      assert.include(net.data[6], JSON.stringify('after close, before connect'));
+      assert.include(net.data[8], JSON.stringify('after close, after connect'));
       assert.equal(net.createConnectionCalled, 2);
       assert.end();
     });
@@ -159,14 +170,18 @@ test('Multiprocess Appender', (batch) => {
   batch.test('worker defaults', (t) => {
     const fakeNet = makeFakeNet();
 
-    sandbox.require(
-      '../../lib/appenders/multiprocess',
+    const log4js = sandbox.require(
+      '../../lib/log4js',
       {
         requires: {
           net: fakeNet
         }
       }
-    ).appender({ mode: 'worker' });
+    );
+    log4js.configure({
+      appenders: { worker: { type: 'multiprocess', mode: 'worker' } },
+      categories: { default: { appenders: ['worker'], level: 'trace' } }
+    });
 
     t.test('should open a socket to localhost:5000', (assert) => {
       assert.equal(fakeNet.port, 5000);
@@ -179,21 +194,28 @@ test('Multiprocess Appender', (batch) => {
   batch.test('master', (t) => {
     const fakeNet = makeFakeNet();
 
-    const appender = sandbox.require(
-      '../../lib/appenders/multiprocess',
+    const log4js = sandbox.require(
+      '../../lib/log4js',
       {
         requires: {
-          net: fakeNet
+          net: fakeNet,
+          './appenders/recording': recording
         }
       }
-    ).appender({
-      mode: 'master',
-      loggerHost: 'server',
-      loggerPort: 1234,
-      actualAppender: fakeNet.fakeAppender.bind(fakeNet)
+    );
+    log4js.configure({
+      appenders: {
+        recorder: { type: 'recording' },
+        master: {
+          type: 'multiprocess',
+          mode: 'master',
+          loggerPort: 1234,
+          loggerHost: 'server',
+          appender: 'recorder'
+        }
+      },
+      categories: { default: { appenders: ['master'], level: 'trace' } }
     });
-
-    appender('this should be sent to the actual appender directly');
 
     const net = fakeNet;
 
@@ -204,7 +226,9 @@ test('Multiprocess Appender', (batch) => {
     });
 
     t.test('should return the underlying appender', (assert) => {
-      assert.equal(net.logEvents[0], 'this should be sent to the actual appender directly');
+      log4js.getLogger().info('this should be sent to the actual appender directly');
+
+      assert.equal(recording.replay()[0].data[0], 'this should be sent to the actual appender directly');
       assert.end();
     });
 
@@ -237,93 +261,104 @@ test('Multiprocess Appender', (batch) => {
       );
       net.cbs.data('bad message__LOG4JS__');
 
+      const logEvents = recording.replay();
       // should parse log messages into log events and send to appender
-      assert.equal(net.logEvents[1].level.toString(), 'ERROR');
-      assert.equal(net.logEvents[1].data[0], 'an error message');
-      assert.equal(net.logEvents[1].remoteAddress, '1.2.3.4');
-      assert.equal(net.logEvents[1].remotePort, '1234');
+      assert.equal(logEvents[0].level.toString(), 'ERROR');
+      assert.equal(logEvents[0].data[0], 'an error message');
+      assert.equal(logEvents[0].remoteAddress, '1.2.3.4');
+      assert.equal(logEvents[0].remotePort, '1234');
 
       // should parse log messages split into multiple chunks'
-      assert.equal(net.logEvents[2].level.toString(), 'DEBUG');
-      assert.equal(net.logEvents[2].data[0], 'some debug');
-      assert.equal(net.logEvents[2].remoteAddress, '1.2.3.4');
-      assert.equal(net.logEvents[2].remotePort, '1234');
+      assert.equal(logEvents[1].level.toString(), 'DEBUG');
+      assert.equal(logEvents[1].data[0], 'some debug');
+      assert.equal(logEvents[1].remoteAddress, '1.2.3.4');
+      assert.equal(logEvents[1].remotePort, '1234');
 
       // should parse multiple log messages in a single chunk'
-      assert.equal(net.logEvents[3].data[0], 'some debug');
-      assert.equal(net.logEvents[4].data[0], 'some debug');
-      assert.equal(net.logEvents[5].data[0], 'some debug');
+      assert.equal(logEvents[2].data[0], 'some debug');
+      assert.equal(logEvents[3].data[0], 'some debug');
+      assert.equal(logEvents[4].data[0], 'some debug');
 
       // should handle log messages sent as part of end event'
-      assert.equal(net.logEvents[6].data[0], "that's all folks");
+      assert.equal(logEvents[5].data[0], "that's all folks");
 
       // should handle unparseable log messages
-      assert.equal(net.logEvents[7].level.toString(), 'ERROR');
-      assert.equal(net.logEvents[7].categoryName, 'log4js');
-      assert.equal(net.logEvents[7].data[0], 'Unable to parse log:');
-      assert.equal(net.logEvents[7].data[1], 'bad message');
+      assert.equal(logEvents[6].level.toString(), 'ERROR');
+      assert.equal(logEvents[6].categoryName, 'log4js');
+      assert.equal(logEvents[6].data[0], 'Unable to parse log:');
+      assert.equal(logEvents[6].data[1], 'bad message');
 
       assert.end();
     });
+    t.end();
+  });
+
+  batch.test('master without actual appender throws error', (t) => {
+    const fakeNet = makeFakeNet();
+
+    const log4js = sandbox.require(
+      '../../lib/log4js',
+      {
+        requires: {
+          net: fakeNet
+        }
+      }
+    );
+    t.throws(() =>
+      log4js.configure({
+        appenders: { master: { type: 'multiprocess', mode: 'master' } },
+        categories: { default: { appenders: ['master'], level: 'trace' } }
+      }),
+      new Error('multiprocess master must have an "appender" defined')
+    );
+    t.end();
+  });
+
+  batch.test('master with unknown appender throws error', (t) => {
+    const fakeNet = makeFakeNet();
+
+    const log4js = sandbox.require(
+      '../../lib/log4js',
+      {
+        requires: {
+          net: fakeNet
+        }
+      }
+    );
+    t.throws(() =>
+      log4js.configure({
+        appenders: { master: { type: 'multiprocess', mode: 'master', appender: 'cheese' } },
+        categories: { default: { appenders: ['master'], level: 'trace' } }
+      }),
+      new Error('multiprocess master appender "cheese" not defined')
+    );
     t.end();
   });
 
   batch.test('master defaults', (t) => {
     const fakeNet = makeFakeNet();
 
-    sandbox.require(
-      '../../lib/appenders/multiprocess',
+    const log4js = sandbox.require(
+      '../../lib/log4js',
       {
         requires: {
           net: fakeNet
         }
       }
-    ).appender({ mode: 'master' });
+    );
+    log4js.configure({
+      appenders: {
+        stdout: { type: 'stdout' },
+        master: { type: 'multiprocess', mode: 'master', appender: 'stdout' }
+      },
+      categories: { default: { appenders: ['master'], level: 'trace' } }
+    });
 
     t.test('should listen for log messages on localhost:5000', (assert) => {
       assert.equal(fakeNet.port, 5000);
       assert.equal(fakeNet.host, 'localhost');
       assert.end();
     });
-    t.end();
-  });
-
-  batch.test('configure', (t) => {
-    const results = {};
-    const fakeNet = makeFakeNet();
-
-    sandbox.require(
-      '../../lib/appenders/multiprocess',
-      {
-        requires: {
-          net: fakeNet,
-          '../log4js': {
-            loadAppender: function (app) {
-              results.appenderLoaded = app;
-            },
-            appenderMakers: {
-              madeupappender: function (config, options) {
-                results.config = config;
-                results.options = options;
-              }
-            }
-          }
-        }
-      }
-    ).configure(
-      {
-        mode: 'master',
-        appender: {
-          type: 'madeupappender',
-          cheese: 'gouda'
-        }
-      },
-      { crackers: 'jacobs' }
-    );
-
-    t.equal(results.appenderLoaded, 'madeupappender', 'should load underlying appender for master');
-    t.equal(results.config.cheese, 'gouda', 'should pass config to underlying appender');
-    t.equal(results.options.crackers, 'jacobs', 'should pass options to underlying appender');
     t.end();
   });
 
