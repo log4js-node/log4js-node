826d47d4c3d936b321ac61d6b10b95c6f83dea25
Gareth Jones <gareth.jones@shinetech.com>
2017-06-13 08:38:34 +1000

    test(stderr): improved coverage

--- test/tap/stderrAppender-test.js
+++ test/tap/stderrAppender-test.js
@@ -28,3 +28,30 @@ test('stderr appender', (t) => {
   t.equal(output[0], 'biscuits\n', 'The message should be biscuits.');
   t.end();
 });
+
+test('stderr appender with default layout', (t) => {
+  const output = [];
+  layouts.colouredLayout = () => 'I used the colouredLayout';
+
+  const appender = sandbox.require(
+    '../../lib/appenders/stderr',
+    {
+      globals: {
+        process: {
+          stderr: {
+            write: function (data) {
+              output.push(data);
+            }
+          }
+        }
+      }
+    }
+  ).configure({ type: 'stderr' }, layouts);
+
+
+  appender({ data: ['biscuits'] });
+  t.plan(2);
+  t.equal(output.length, 1, 'There should be one message.');
+  t.equal(output[0], 'I used the colouredLayout\n', 'The message should have gone through the default layout.');
+  t.end();
+});
