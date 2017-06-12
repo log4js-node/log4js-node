8e0ce26e44840d5af8b4a6c8ea7daed254ccb12b
Gareth Jones <gareth.jones@shinetech.com>
2017-06-13 08:39:56 +1000

    fix(multiprocess): worker shutdown fix, and updated example

--- examples/example-socket.js
+++ examples/example-socket.js
@@ -1,45 +1,48 @@
-var log4js = require('./lib/log4js')
-, cluster = require('cluster')
-, numCPUs = require('os').cpus().length
-, i = 0;
+const log4js = require('../lib/log4js');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+let i = 0;
 
 if (cluster.isMaster) {
-    log4js.configure({
-        appenders: [
-            {
-                type: "multiprocess",
-                mode: "master",
-                appender: {
-                    type: "console"
-                }
-            }
-        ]
-    });
-
-    console.info("Master creating %d workers", numCPUs);
-    for (i=0; i < numCPUs; i++) {
-        cluster.fork();
+  log4js.configure({
+    appenders: {
+      console: { type: 'console' },
+      master: {
+        type: 'multiprocess',
+        mode: 'master',
+        appender: 'console'
+      }
+    },
+    categories: {
+      default: { appenders: ['console'], level: 'info' }
     }
+  });
 
-    cluster.on('death', function(worker) {
-        console.info("Worker %d died.", worker.pid);
-    });
+  console.info('Master creating %d workers', numCPUs);
+  for (i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('death', (worker) => {
+    console.info('Worker %d died.', worker.pid);
+  });
 } else {
-    log4js.configure({
-        appenders: [
-            {
-                type: "multiprocess",
-                mode: "worker"
-            }
-        ]
-    });
-    var logger = log4js.getLogger('example-socket');
-
-    console.info("Worker %d started.", process.pid);
-    for (i=0; i < 1000; i++) {
-        logger.info("Worker %d - logging something %d", process.pid, i);
+  log4js.configure({
+    appenders: {
+      worker: { type: 'multiprocess', mode: 'worker' }
+    },
+    categories: {
+      default: { appenders: ['worker'], level: 'info' }
     }
+  });
+  const logger = log4js.getLogger('example-socket');
+
+  console.info('Worker %d started.', process.pid);
+  for (i = 0; i < 1000; i++) {
+    logger.info('Worker %d - logging something %d', process.pid, i);
+  }
+  log4js.shutdown(() => {
+    process.exit();
+  });
 }
-
-
-
