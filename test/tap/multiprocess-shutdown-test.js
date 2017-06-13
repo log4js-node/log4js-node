5eda1d367ac3a3b1c93ec09dda517918f487da99
Gareth Jones <gareth.jones@shinetech.com>
2017-06-14 08:37:20 +1000

    test(multiprocess): added test for worker shutdown

--- test/tap/multiprocess-shutdown-test.js
+++ test/tap/multiprocess-shutdown-test.js
@@ -3,6 +3,7 @@
 const test = require('tap').test;
 const log4js = require('../../lib/log4js');
 const net = require('net');
+const sandbox = require('sandboxed-module');
 
 test('multiprocess appender shutdown (master)', { timeout: 2000 }, (t) => {
   log4js.configure({
@@ -28,5 +29,60 @@ test('multiprocess appender shutdown (master)', { timeout: 2000 }, (t) => {
         t.end();
       });
     });
+  }, 500);
+});
+
+test('multiprocess appender shutdown (worker)', (t) => {
+  const fakeConnection = {
+    evts: {},
+    msgs: [],
+    on: function (evt, cb) {
+      this.evts[evt] = cb;
+    },
+    write: function (data) {
+      this.msgs.push(data);
+    },
+    removeAllListeners: function () {
+      this.removeAllListenersCalled = true;
+    },
+    end: function (cb) {
+      this.endCb = cb;
+    }
+  };
+  const logLib = sandbox.require('../../lib/log4js', {
+    requires: {
+      net: {
+        createConnection: function () {
+          return fakeConnection;
+        }
+      }
+    }
+  });
+  logLib.configure({
+    appenders: { worker: { type: 'multiprocess', mode: 'worker' } },
+    categories: { default: { appenders: ['worker'], level: 'debug' } }
+  });
+
+  logLib.getLogger().info('Putting something in the buffer before the connection is established');
+  // nothing been written yet.
+  t.equal(fakeConnection.msgs.length, 0);
+
+  let shutdownFinished = false;
+  logLib.shutdown(() => {
+    shutdownFinished = true;
+  });
+
+  // still nothing been written yet.
+  t.equal(fakeConnection.msgs.length, 0);
+
+  fakeConnection.evts.connect();
+
+  setTimeout(() => {
+    t.equal(fakeConnection.msgs.length, 2);
+    t.ok(fakeConnection.removeAllListenersCalled);
+    fakeConnection.endCb();
+
+    t.ok(shutdownFinished);
+    t.end();
   }, 500);
 });
