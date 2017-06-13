4a994be8ff70e5550d70ec874c462589d53893c1
Gareth Jones <gareth.jones@shinetech.com>
2017-06-14 08:51:24 +1000

    chore(examples): updated hipchat example for 2.x

--- examples/hipchat-appender.js
+++ examples/hipchat-appender.js
@@ -4,51 +4,53 @@
  *   - npm install hipchat-notifier
  */
 
-var log4js = require('../lib/log4js');
+const log4js = require('../lib/log4js');
 
 log4js.configure({
-  "appenders": [
-    {
-      "type" : "hipchat",
-      "hipchat_token": process.env.HIPCHAT_TOKEN || '< User token with Notification Privileges >',
-      "hipchat_room": process.env.HIPCHAT_ROOM || '< Room ID or Name >'
+  appenders: {
+    hipchat: {
+      type: 'hipchat',
+      hipchat_token: process.env.HIPCHAT_TOKEN || '< User token with Notification Privileges >',
+      hipchat_room: process.env.HIPCHAT_ROOM || '< Room ID or Name >'
     }
-  ]
+  },
+  categories: {
+    default: { appenders: ['hipchat'], level: 'trace' }
+  }
 });
 
-var logger = log4js.getLogger("hipchat");
-logger.warn("Test Warn message");
-logger.info("Test Info message");
-logger.debug("Test Debug Message");
-logger.trace("Test Trace Message");
-logger.fatal("Test Fatal Message");
-logger.error("Test Error Message");
+const logger = log4js.getLogger('hipchat');
+logger.warn('Test Warn message');
+logger.info('Test Info message');
+logger.debug('Test Debug Message');
+logger.trace('Test Trace Message');
+logger.fatal('Test Fatal Message');
+logger.error('Test Error Message');
 
 
 // alternative configuration demonstrating callback + custom layout
-///////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////
 
 // use a custom layout function (in this case, the provided basicLayout)
 //   format: [TIMESTAMP][LEVEL][category] - [message]
-var customLayout = require('../lib/layouts').basicLayout;
-
 log4js.configure({
-  "appenders": [
-    {
-      "type" : "hipchat",
-      "hipchat_token": process.env.HIPCHAT_TOKEN || '< User token with Notification Privileges >',
-      "hipchat_room": process.env.HIPCHAT_ROOM || '< Room ID or Name >',
-      "hipchat_from": "Mr. Semantics",
-      "hipchat_notify": false,
-      "hipchat_response_callback": function(err, response, body){
-        if(err || response.statusCode > 300){
+  appenders: {
+    hipchat: {
+      type: 'hipchat',
+      hipchat_token: process.env.HIPCHAT_TOKEN || '< User token with Notification Privileges >',
+      hipchat_room: process.env.HIPCHAT_ROOM || '< Room ID or Name >',
+      hipchat_from: 'Mr. Semantics',
+      hipchat_notify: false,
+      hipchat_response_callback: function (err, response, body) {
+        if (err || response.statusCode > 300) {
           throw new Error('hipchat-notifier failed');
         }
         console.log('mr semantics callback success');
       },
-      "layout": customLayout
+      layout: { type: 'basic' }
     }
-  ]
+  },
+  categories: { default: { appenders: ['hipchat'], level: 'trace' } }
 });
 
-logger.info("Test customLayout from Mr. Semantics");
+logger.info('Test customLayout from Mr. Semantics');
