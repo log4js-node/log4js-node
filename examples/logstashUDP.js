7e3bbe4b3fa8bdb7103149fa427d56b6b238a3f4
Gareth Jones <gareth.jones@shinetech.com>
2017-06-15 08:12:41 +1000

    chore(examples): updated logstash example for 2.x

--- examples/logstashUDP.js
+++ examples/logstashUDP.js
@@ -1,4 +1,4 @@
-var log4js = require('../lib/log4js');
+const log4js = require('../lib/log4js');
 
 /*
  Sample logstash config:
@@ -12,28 +12,29 @@ var log4js = require('../lib/log4js');
 */
 
 log4js.configure({
-  "appenders": [
-    {
-      type: "console",
-      category: "myLogger"
+  appenders: {
+    console: {
+      type: 'console'
     },
-    {
-      "host": "127.0.0.1",
-      "port": 10001,
-      "type": "logstashUDP",
-      "logType": "myAppType", // Optional, defaults to 'category'
-      "fields": {             // Optional, will be added to the 'fields' object in logstash
-        "field1": "value1",
-        "field2": "value2"
+    logstash: {
+      host: '127.0.0.1',
+      port: 10001,
+      type: 'logstashUDP',
+      logType: 'myAppType', // Optional, defaults to 'category'
+      fields: {             // Optional, will be added to the 'fields' object in logstash
+        field1: 'value1',
+        field2: 'value2'
       },
-      "layout": {
-        "type": "pattern",
-        "pattern": "%m"
-      },
-      "category": "myLogger"
+      layout: {
+        type: 'pattern',
+        pattern: '%m'
+      }
     }
-  ]
+  },
+  categories: {
+    default: { appenders: ['console', 'logstash'], level: 'info' }
+  }
 });
 
-var logger = log4js.getLogger("myLogger");
-logger.info("Test log message %s", "arg1", "arg2");
+const logger = log4js.getLogger('myLogger');
+logger.info('Test log message %s', 'arg1', 'arg2');
