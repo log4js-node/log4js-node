cffe5bb44b8b98d530d85e14065be0b3045b333f
Gareth Jones <gareth.jones@shinetech.com>
2017-06-15 08:08:02 +1000

    chore(examples): updated logFaces example for 2.x

--- examples/logFaces-appender.js
+++ examples/logFaces-appender.js
@@ -1,4 +1,4 @@
-var log4js = require('../lib/log4js');
+const log4js = require('../lib/log4js');
 
 /*
    logFaces server configured with UDP receiver, using JSON format,
@@ -6,19 +6,20 @@ var log4js = require('../lib/log4js');
 */
 
 log4js.configure({
-  "appenders": [
-    {
-      "type": "logFacesAppender",       // (mandatory) appender type
-      "application": "MY-NODEJS",       // (optional) name of the application (domain)
-      "remoteHost": "localhost",        // (optional) logFaces server host or IP address
-      "port": 55201,                    // (optional) logFaces UDP receiver port (must use JSON format)  
-      "layout": {                       // (optional) the layout to use for messages
-        "type": "pattern",
-        "pattern": "%m"
-      }       
+  appenders: {
+    logFaces: {
+      type: 'logFaces-UDP',       // (mandatory) appender type
+      application: 'MY-NODEJS',       // (optional) name of the application (domain)
+      remoteHost: 'localhost',        // (optional) logFaces server host or IP address
+      port: 55201,                    // (optional) logFaces UDP receiver port (must use JSON format)
+      layout: {                       // (optional) the layout to use for messages
+        type: 'pattern',
+        pattern: '%m'
+      }
     }
-  ]
+  },
+  categories: { default: { appenders: ['logFaces'], level: 'info' } }
 });
 
-var logger = log4js.getLogger("myLogger");
-logger.info("Testing message %s", "arg1");
+const logger = log4js.getLogger('myLogger');
+logger.info('Testing message %s', 'arg1');
