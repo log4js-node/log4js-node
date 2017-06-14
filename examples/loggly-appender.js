477849b7a0366d8c0408fff34d1deb61f8c8fcbf
Gareth Jones <gareth.jones@shinetech.com>
2017-06-15 08:10:24 +1000

    chore(examples): updated loggly example for 2.x

--- examples/loggly-appender.js
+++ examples/loggly-appender.js
@@ -1,24 +1,26 @@
-//Note that loggly appender needs node-loggly to work.
-//If you haven't got node-loggly installed, you'll get cryptic
-//"cannot find module" errors when using the loggly appender
-var log4js = require('../lib/log4js');
+// Note that loggly appender needs node-loggly to work.
+// If you haven't got node-loggly installed, you'll get cryptic
+// "cannot find module" errors when using the loggly appender
+const log4js = require('../lib/log4js');
 
 log4js.configure({
-  "appenders": [
-    {
-      type: "console",
-      category: "test"
+  appenders: {
+    console: {
+      type: 'console'
     },
-    {
-      "type"     : "loggly",
-      "token"    : "12345678901234567890",
-      "subdomain": "your-subdomain",
-      "tags"     : ["test"],
-      "category" : "loggly"
+    loggly: {
+      type: 'loggly',
+      token: '12345678901234567890',
+      subdomain: 'your-subdomain',
+      tags: ['test']
     }
-  ]
+  },
+  categories: {
+    default: { appenders: ['console'], level: 'info' },
+    loggly: { appenders: ['loggly'], level: 'info' }
+  }
 });
 
-var logger = log4js.getLogger("loggly");
-logger.info("Test log message");
-//logger.debug("Test log message");
+const logger = log4js.getLogger('loggly');
+logger.info('Test log message');
+// logger.debug("Test log message");
