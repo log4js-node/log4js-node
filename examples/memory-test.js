6933c4064da3eb119bf4a9f03b19c916bd126464
Gareth Jones <gareth.jones@shinetech.com>
2017-06-21 08:49:51 +1000

    fix(performance): updated streamroller version to improve file perf #486

--- examples/memory-test.js
+++ examples/memory-test.js
@@ -1,37 +1,37 @@
-var log4js = require('../lib/log4js')
-, logger
-, usage
-, i;
+const log4js = require('../lib/log4js');
 
 log4js.configure(
-    {
-        appenders: [
-            {
-                category: "memory-test"
-              , type: "file"
-              , filename: "memory-test.log"
-            },
-            {
-                type: "console"
-              , category: "memory-usage"
-            },
-            {
-                type: "file"
-              , filename: "memory-usage.log"
-              , category: "memory-usage"
-              , layout: {
-                  type: "messagePassThrough"
-              }
-            }
-        ]
+  {
+    appenders: {
+      logs: {
+        type: 'file',
+        filename: 'memory-test.log'
+      },
+      console: {
+        type: 'stdout',
+      },
+      file: {
+        type: 'file',
+        filename: 'memory-usage.log',
+        layout: {
+          type: 'messagePassThrough'
+        }
+      }
+    },
+    categories: {
+      default: { appenders: ['console'], level: 'info' },
+      'memory-test': { appenders: ['logs'], level: 'info' },
+      'memory-usage': { appenders: ['console', 'file'], level: 'info' }
     }
+  }
 );
-logger = log4js.getLogger("memory-test");
-usage = log4js.getLogger("memory-usage");
+const logger = log4js.getLogger('memory-test');
+const usage = log4js.getLogger('memory-usage');
 
-for (i=0; i < 1000000; i++) {
-    if ( (i % 5000) === 0) {
-        usage.info("%d %d", i, process.memoryUsage().rss);
-    }
-    logger.info("Doing something.");
+for (let i = 0; i < 1000000; i += 1) {
+  if ((i % 5000) === 0) {
+    usage.info('%d %d', i, process.memoryUsage().rss);
+  }
+  logger.info('Doing something.');
 }
+log4js.shutdown(() => {});
