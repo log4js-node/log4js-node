log4js-node/log4js-node

commit 6746e2e0488f3c900fd358af0a50d4795df65de7

remove the underscore dependency

lib/appenders/logstashUDP.js
@@ -1,8 +1,7 @@
 "use strict";
 var layouts = require('../layouts')
 , dgram = require('dgram')
-, util = require('util')
-, _ = require('underscore');
+, util = require('util');
 
 function logstashUDP (config, layout) {
   var udp = dgram.createSocket('udp4');
@@ -27,14 +26,20 @@ function logstashUDP (config, layout) {
       Every other field is valid and fine.
     */
 
+    if (loggingEvent.data.length > 1) {
+      var secondEvData = loggingEvent.data[1];
+      for (var k in secondEvData) {
+        config.fields[k] = secondEvData[k];
+      }
+    }
+    config.fields.level = loggingEvent.level.levelStr;
+
     var logObject = {
       "@version" : "1",
       "@timestamp" : (new Date(loggingEvent.startTime)).toISOString(),
       "type" : config.logType ? config.logType : config.category,
       "message" : layout(loggingEvent),
-      "fields" : _.extend(config.fields,
-                    loggingEvent.data.length > 1 ? loggingEvent.data[1] : {},
-                    {'level':loggingEvent.level.levelStr})
+      "fields" : config.fields
     };
     sendLog(udp, config.host, config.port, logObject);
   };

lib/connect-logger.js
@@ -1,6 +1,5 @@
 "use strict";
 var levels = require("./levels");
-var _ = require('underscore');
 var DEFAULT_FORMAT = ':remote-addr - -' +
   ' ":method :url HTTP/:http-version"' +
   ' :status :content-length ":referrer"' +
@@ -164,9 +163,10 @@ function assemble_tokens(req, res, custom_tokens) {
  */
 
 function format(str, tokens) {
-  return _.reduce(tokens, function(current_string, token) {
-    return current_string.replace(token.token, token.replacement);
-  }, str);
+  for (var i = 0; i < tokens.length; i++) {
+    str = str.replace(tokens[i].token, tokens[i].replacement);
+  }
+  return str;
 }
 
 /**

commit 31c8a2d8d1450b48a4f7cb7774829705ffa9b1b3

fix: sticky fields
Fixed an issue that was introduced in commit 6746e2e0488f3c900fd358af0a50d4795df65de7
All fields became sticky, so each time a field was sent via a message, it showed up in all messages after that

lib/appenders/logstashUDP.js
@@ -37,28 +37,34 @@ function logstashUDP(config, layout) {
      @version is the version number of this json schema
      Every other field is valid and fine.
      */
+
+    const fields = {};
+    Object.keys(config.fields).forEach((key) => {
+      fields[key] = config.fields[key];
+    });
+
     /* eslint no-prototype-builtins:1,no-restricted-syntax:[1, "ForInStatement"] */
     if (loggingEvent.data.length > 1) {
       const secondEvData = loggingEvent.data[1];
       Object.keys(secondEvData).forEach((key) => {
-        config.fields[key] = secondEvData[key];
+        fields[key] = secondEvData[key];
       });
     }
-    config.fields.level = loggingEvent.level.levelStr;
-    config.fields.category = loggingEvent.categoryName;
+    fields.level = loggingEvent.level.levelStr;
+    fields.category = loggingEvent.categoryName;
 
     const logObject = {
       '@version': '1',
       '@timestamp': (new Date(loggingEvent.startTime)).toISOString(),
       type: type,
       message: layout(loggingEvent),
-      fields: config.fields
+      fields: fields
     };
 
-    const keys = Object.keys(config.fields);
-    for (let i = 0, length = keys.length; i < length; i += 1) {
-      logObject[keys[i]] = config.fields[keys[i]];
-    }
+    Object.keys(fields).forEach((key) => {
+      logObject[key] = fields[key];
+    });
+
     sendLog(udp, config.host, config.port, logObject);
   }
 