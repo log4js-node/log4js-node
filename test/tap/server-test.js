319562914b9a759dc02e98efa3154684ed81bacb
Gareth Jones <gareth.jones@shinetech.com>
2017-12-12 08:03:57 +1100

    fix(tcp): interim commit while I work on something else

--- test/tap/server-test.js
+++ test/tap/server-test.js
@@ -1,6 +1,44 @@
 const test = require('tap').test;
+const net = require('net');
+const log4js = require('../../lib/log4js');
+const vcr = require('../../lib/appenders/recording');
+const levels = require('../../lib/levels')();
+const LoggingEvent = (require('../../lib/logger')(levels)).LoggingEvent;
+
+log4js.configure({
+  appenders: {
+    vcr: { type: 'recording' }
+  },
+  categories: {
+    default: { appenders: ['vcr'], level: 'debug' }
+  },
+  listen: {
+    port: 5678
+  }
+});
 
 test('TCP Server', (batch) => {
-  batch.test('should listen for TCP messages and re-send via process.send');
+  batch.test('should listen for TCP messages and re-send via process.send', (t) => {
+    const socket = net.connect(5678, () => {
+      socket.write(
+        (new LoggingEvent('test-category', levels.INFO, ['something'], {})).serialise(),
+        () => {
+          socket.end();
+          log4js.shutdown(() => {
+            const logs = vcr.replay();
+            t.equal(logs.length, 1);
+            t.match(logs[0], {
+              data: ['something'],
+              categoryName: 'test-category',
+              level: { levelStr: 'INFO' },
+              context: {}
+            });
+            t.end();
+          });
+        }
+      );
+    });
+    socket.unref();
+  });
   batch.end();
 });
