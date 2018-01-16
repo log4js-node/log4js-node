ef2cd688cb6b83d9beec9e060aec71bef3698e65
anteoy <anteoy@gmail.com>
2018-01-16 15:25:52 +0800

    feat(rabbitmq): Added ability to push to rabbitmq

---
+++ examples/rabbitmq-appender.js
@@ -0,0 +1,50 @@
+// Note that rabbitmq appender needs install amqplib to work.
+
+const log4js = require('../lib/log4js');
+
+log4js.configure({
+  appenders: {
+    out: {
+      type: 'console'
+    },
+    file: {
+      type: 'dateFile',
+      filename: 'logs/log.txt',
+      pattern: 'yyyyMMdd',
+      alwaysIncludePattern: false
+    },
+    mq: {
+      type: 'rabbitmq',
+      host: '127.0.0.1',
+      port: 5672,
+      username: 'guest',
+      password: 'guest',
+      routing_key: 'logstash',
+      exchange: 'exchange_logs',
+      mq_type: 'direct',
+      durable: true,
+      channel: 'q_log',
+      layout: {
+        type: 'pattern',
+        pattern: '%d{yyyy-MM-dd hh:mm:ss:SSS}#%p#%m'
+      }
+    }
+  },
+  categories: {
+    default: { appenders: ['out'], level: 'info' },
+    dateFile: { appenders: ['file'], level: 'info' },
+    rabbitmq: { appenders: ['mq'], level: 'info' }
+  }
+});
+
+const log = log4js.getLogger('console');
+const logRabbitmq = log4js.getLogger('rabbitmq');
+
+function doTheLogging(x) {
+  log.info('Logging something %d', x);
+  logRabbitmq.info('Logging something %d', x);
+}
+
+for (let i = 0; i < 500; i += 1) {
+  doTheLogging(i);
+}
