a859fe4ab0f3990781efb786c9888f033c827a21
Gareth Jones <gareth.jones@shinetech.com>
2019-11-19 08:27:24 +1100

    chore: made language of pause and resume a bit clearer in test

--- test/tap/pause-test.js
+++ test/tap/pause-test.js
@@ -4,31 +4,32 @@ const log4js = require("../../lib/log4js");
 tap.test("Drain event test", batch => {
 
   batch.test("Should emit pause event and resume when logging in a file with high frequency", t => {
-    // Generate logger with 5MB of highWaterMark config
+    // Generate logger with 5k of highWaterMark config
     log4js.configure({
       appenders: {
-        file: { type: "file", filename: "logs/drain.log", highWaterMark: 5 * 1024 * 1024 }
+        file: { type: "file", filename: "logs/drain.log", highWaterMark: 5 * 1024  }
       },
       categories: {
         default: { appenders: ["file"], level: "debug" }
       }
     });
 
-    let onPause = false;
-    let onResume = false;
+    let paused = false;
+    let resumed = false;
 
     process.on("log4js:pause", value => {
       if (value) {
-        onPause = true;
+        paused = true;
       } else {
-        onResume = true;
+        resumed = true;
       }
     });
 
     const logger = log4js.getLogger();
-    while (onPause === false && onResume === false) {
-      if (onPause === false)
+    while (!paused && !resumed) {
+      if (!paused) {
         logger.info("This is a test for emitting drain event");
+      }
     }
     t.end();
   });
