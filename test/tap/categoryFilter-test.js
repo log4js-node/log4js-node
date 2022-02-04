654710bd84227b79989353c57c1986fe7d9d029a
Lam Wei Li <peteriman@mail.com>
2022-02-04 17:06:22 +0800

    chore(test): improve test (branch) coverage for categoryFilter
    appenders/categoryFilter.js - Line 4 - if (typeof excludes === 'string') excludes = [excludes];

--- test/tap/categoryFilter-test.js
+++ test/tap/categoryFilter-test.js
@@ -40,6 +40,38 @@ test("log4js categoryFilter", batch => {
     t.end();
   });
 
+  batch.test("appender should exclude categories", t => {
+    log4js.configure({
+      appenders: {
+        recorder: { type: "recording" },
+        filtered: {
+          type: "categoryFilter",
+          exclude: ["app", "web"],
+          appender: "recorder"
+        }
+      },
+      categories: { default: { appenders: ["filtered"], level: "DEBUG" } }
+    });
+
+    const webLogger = log4js.getLogger("web");
+    const appLogger = log4js.getLogger("app");
+
+    webLogger.debug("This should not get logged");
+    appLogger.debug("This should get logged");
+    webLogger.debug("Hello again");
+    log4js
+      .getLogger("db")
+      .debug("This should be included by the appender anyway");
+
+    const logEvents = recording.replay();
+    t.equal(logEvents.length, 1);
+    t.equal(
+      logEvents[0].data[0],
+      "This should be included by the appender anyway"
+    );
+    t.end();
+  });
+
   batch.test("should not really need a category filter any more", t => {
     log4js.configure({
       appenders: { recorder: { type: "recording" } },
